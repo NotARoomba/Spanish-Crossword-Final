7da3c6e97eafc7bfb00a1e86bd885b89929f6086
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2024-03-01 21:24:30 -0500

    text center

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -5,8 +5,8 @@ export default function App () {
   // const [count, setCount] = useState(0)
   return (
     <div className="w-full h-full flex flex-col">
-      <p className="mt-10 text-4xl mx-auto font-bold ">El Extraño Caso del Dr. Jekyll y Mr. Hyde</p>
-      <p className="text-2xl mx-auto font-bold text-blue-500">Hecho por Nathan Alspaugh</p>
+      <p className="mt-10 text-4xl mx-auto font-bold text-center ">El Extraño Caso del Dr. Jekyll y Mr. Hyde</p>
+      <p className="text-2xl mx-auto font-bold text-blue-500 text-center">Hecho por Nathan Alspaugh</p>
       {/* <div className='w-1/2 h-1/2'> */}
         {/* <Crossword data={data} /> */}
         <iframe width={window.innerWidth} height={window.innerHeight} className=' m-auto block bg-transparent border-none' src="https://crosswordlabs.com/embed/2024-03-01-693"></iframe>
